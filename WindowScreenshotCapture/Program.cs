11c9a69973994c184080fa4db58a63f262d6f919
nko <nko@DEV25>
2017-06-06 15:28:38 +0300

    directory

--- WindowScreenshotCapture/Program.cs
+++ WindowScreenshotCapture/Program.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace WindowScreenshotCapture
 {
     class Program
     {
-        private static string _folder = @"D:\Screenshots\";
+        private static string _folder = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Screenshots");
 
         static void Main(string[] args)
         {
@@ -27,8 +24,21 @@ namespace WindowScreenshotCapture
             {
                 if (!Directory.Exists(_folder))
                     Directory.CreateDirectory(_folder);
+                
+                try
+                {
+                    var screenShotName = Path.Combine(_folder, DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss"));
 
-                ScreenCapturer.CaptureAndSave($"{Path.Combine(_folder, DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss"))}");
+                    ScreenCapturer.CaptureAndSave($"{screenShotName}");
+
+                    Console.WriteLine($"Successfully created screenshot {screenShotName}");
+                }
+                catch
+                {
+                    Console.WriteLine($"Failed to save screenshot");
+
+                    throw;
+                }
             }
         }
     }
